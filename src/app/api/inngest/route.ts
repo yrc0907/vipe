a13be0a491f08f2eaeb4c64db376bbd6321df5ac
yrc0907 <3616104980@qq.com>
2025-06-28 10:47:41 +0800

    first commit

---
+++ src/app/api/inngest/route.ts
@@ -0,0 +1,13 @@
+import { inngest } from "@/inngest/client";
+import { helloWorld } from "@/inngest/functions";
+import { serve } from "inngest/next";
+
+
+// Create an API that serves zero functions
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    /* your functions will be passed here later! */
+    helloWorld
+  ],
+});
